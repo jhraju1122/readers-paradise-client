3534052a2f9cfbf17f99a20b891dac8c17b0a99a
jhraju <jhraju890@gmail.com>
2025-01-12 17:41:49 +0600

    added category page

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,33 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+ 
+import horror2 from '../../../assets/img/horror3.webp'
+import horror3 from '../../../assets/img/horror3.webp'
+import horror4 from '../../../assets/img/horror4.jpg'
+import horror5 from '../../../assets/img/horror5.webp'
+const Category = () => {
+    return (
+        <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+      
+        <SwiperSlide><img src={horror2} alt="" /><h3 className='text-4xl uppercase text-center'>Jongol Barir dakura</h3></SwiperSlide>
+        <SwiperSlide><img src={horror3} alt="" /><h3 className='text-4xl uppercase text-center'>Jongol Barir dakura</h3></SwiperSlide>
+        <SwiperSlide><img src={horror4} alt="" /><h3 className='text-4xl uppercase text-center'>Jongol Barir dakura</h3></SwiperSlide>
+        <SwiperSlide><img src={horror5} alt="" /><h3 className='text-4xl uppercase text-center'>Jongol Barir dakura</h3></SwiperSlide>
+       
+      </Swiper>
+    );
+};
+
+export default Category;
