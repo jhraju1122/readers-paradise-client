96fb4cadaf56defc48b268b7186b1d61d79a83f4
jhraju <jhraju890@gmail.com>
2025-01-14 00:09:23 +0600

    added add book page

--- src/pages/Shared/AddBooks/AddBooks.jsx
+++ src/pages/Shared/AddBooks/AddBooks.jsx
@@ -2,33 +2,59 @@ import { Link } from "react-router-dom";
 
  
 const AddBooks = () => {
-    const handleAddBook = event =>{
-        event.preventDefault();
-       
+    const handleAddBook = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = e.target.name.value;
+        const quantity = e.target.quantity.value;
+        const author = e.target.author.value;
+        const details = e.target.details.value;
+        const price = e.target.price.value;
+        const category = e.target.category.value;
+        const photo = e.target.photo.value;
+     
+        const addBook = {name, quantity, author, details, price, category, photo};
+        console.log(addBook);
     }
 
     return (
-        <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-
-            <form className="card-body">
-              <div className="form-control">
-               <h1 className="text-3xl text-slate-400 text-center mt-5 mb-5">Add Book</h1>
-                <input type="text" placeholder="Name" name="name" className="input input-bordered" required />
-              </div>
-              <div className="form-control">
-                <input type="number" placeholder="Quantity" name="Number" className="input input-bordered" required />
-              </div>
-              <div className="form-control mt-6">
-                <Link className="btn btn-primary" type="submit">Add Book</Link>
-              </div>
-            </form>
-
-          </div>
-        </div>
+      <div className="bg-[#F4F3F0] p-24">
+      <h1 className="mb-5 text-slate-400 text-3xl font-extrabold">add a Book</h1>
+      <form onSubmit={handleAddBook} >
+         {/* form name and Quantity row  */}
+      <div className="md:flex w-[100%] justify-center mb-3 gap-3">
+      <div className="input input-bordered flex items-center gap-2 md:w-1/2">
+      <input type="text" className="grow" name="name" placeholder="Book Name" />
       </div>
+      <div className="input input-bordered flex items-center gap-2 md:w-1/2">
+      <input type="text" className="grow" name="quantity" placeholder="Available Quantity" />
+      </div >
+      </div>
+         {/* form supplier row  */}
+      <div className="md:flex w-[100%] justify-center mb-3 gap-3">
+      <div className="input input-bordered flex items-center gap-2 md:w-1/2">
+      <input type="text" className="grow" name="author" placeholder="Author" />
+      </div>
+      <div className="input input-bordered flex items-center gap-2 md:w-1/2">
+      <input type="text" className="grow" name="details" placeholder="Details" />
+      </div >
+      </div>
+         {/* form teste row  */}
+      <div className="md:flex w-[100%] justify-center mb-3 gap-3">
+      <div className="input input-bordered flex items-center gap-2 md:w-1/2">
+      <input type="text" className="grow" name="price" placeholder="price" />
+      </div>
+      <div className="input input-bordered flex items-center gap-2 md:w-1/2">
+      <input type="text" className="grow" name="category" placeholder="Category" />
+      </div >
+      </div>
+      <div className="input input-bordered flex items-center gap-2 md:w-full">
+      <input type="text" className="grow text-center" name="photo"  placeholder="Photo Url" />
+      </div >
+
+          <button type='submit' className="btn mt-5 w-full font-bold text-xl">Add Book</button>
+      </form>
+     </div>
     );
 };
 
