7d6a4c71dc5b050bb6188449b1e13133da56c702
jhraju <jhraju890@gmail.com>
2025-01-16 15:06:37 +0600

    added register connected to forebase

--- src/pages/Shared/Register/Register.jsx
+++ src/pages/Shared/Register/Register.jsx
@@ -1,23 +1,31 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../../../providers/AuthProvider";
 
  
 
 
 
 const Register = () => {
+
+
   const {
     register,
     handleSubmit,
-    watch,
+     
     formState: { errors },
   } = useForm();
 
+  const {createUser}  =useContext(AuthContext);
+
   const onSubmit = (data) =>{
     console.log(data)
+    createUser(data.email, data.password)
+    .then(result =>{
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    })
   }
-
-  
-  console.log(watch("example"))
   
   return (
     <div>
@@ -36,25 +44,31 @@ const Register = () => {
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="name"  {...register("name", { required: true })} name="name" placeholder="name" className="input input-bordered" required />
-        </div>
+          <input type="name"  {...register("name", { required: true })}   placeholder="name" className="input input-bordered" required />
+         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" required />
+          <input type="email"  {...register("email", { required: true })}   placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password"  {...register("password", { required: true })} name="password" placeholder="password" className="input input-bordered" required />
+          <input type="password"  {...register("password", { required: true, minLength: 6, maxLength: 20, pattern: /^[A-Za-z]+$/i })}  placeholder="password" className="input input-bordered" required />
+          {errors.password?.type === "minLength" && (
+        <p role="alert">password must be 6 character </p>
+          )}
+          {errors.password?.type === "maxLength" && (
+        <p role="alert">password must have 20 character </p>
+          )}
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-primary">Register</button>
         </div>
       </form>
     </div>
